44eb5d5587990fd45e21167aea5c5b3cdae76d05
ThomasW69 <ThomasW69@users.noreply.github.com>
2022-10-23 10:41:11 +0200

    5

--- SUNxxKSG04LP3/module.php
+++ SUNxxKSG04LP3/module.php
@@ -16,21 +16,6 @@ declare(strict_types=1);
  */
 require_once __DIR__ . '/../libs/DeyeModule.php';  // diverse Klassen
 
-/*
-        $this->RegisterProfileFloat('VaR', '', '', ' VAr', 0, 0, 0, 2);
-        $this->RegisterProfileFloat('VA', '', '', ' VA', 0, 0, 0, 2);
-        $this->RegisterProfileFloat('PhaseAngle', '', '', ' °', 0, 0, 0, 2);
-        $this->RegisterProfileFloat('Intensity.F', '', '', ' %', 0, 100, 0, 2);
-        $this->RegisterProfileFloat('kVArh', '', '', ' kVArh', 0, 100, 0, 2);
-        $this->RegisterProfileInteger('Volt.I', 'Electricity', '', ' V', 0, 0, 0);
-        $this->RegisterProfileInteger('Hertz.I', 'Electricity', '', ' Hz', 0, 0, 0);
-        $this->RegisterProfileInteger('Ampere.I', 'Electricity', '', ' A', 0, 0, 0);
-        $this->RegisterProfileInteger('Watt.I', 'Electricity', '', ' W', 0, 0, 0);
-        $this->RegisterProfileInteger('VaR.I', '', '', ' VAr', 0, 0, 0);
-        $this->RegisterProfileInteger('VA.I', '', '', ' VA', 0, 0, 0);
-        $this->RegisterProfileInteger('Electricity.I', '', '', ' kWh', 0, 0, 0);
-        $this->RegisterProfileInteger('Percent', '', '', ' %', 0, 0, 0);
-*/
 
 
 /**
@@ -49,39 +34,39 @@ class SUNxxKSG04LP3 extends Deye
     ['Protocol_Ver', 	        VARIABLETYPE_STRING,    VALTYPE_STRING,	    '', 		 2,     3, 1,  0.1,      0,  true],  //Protokollversion
     ['Serial_Number', 	        VARIABLETYPE_STRING,    VALTYPE_ASTRING,	'', 		 3,     3, 5,  0.1,      0,  true],  //Seriennummer
     ['Health_Status',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    '', 	   500,     3, 1,    1,  0,  true], // HealtStatus 0=Standby, 1=SelfCheck, 2=Normal, 3=Alarm, 4=Fault
-    ['Day_ActGen_Power',      	VARIABLETYPE_FLOAT,     VALTYPE_SWORD,	    'Electricity.I', 	   501,     3, 1,  0.1,  0,  true], // Gesamterzeugung heute 
-    ['Day_Bat_Charge',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   514,     3, 1,  0.1,  0,  true], // //Batterieladung heute kWh
-    ['Total_Bat_Charge',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   516,     3, 2,  0.1,  0,  true], // Batterieladung gesamt [kWh]
-    ['Total_Bat_Discharge',     VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   518,     3, 2, 0.01,  0,  true], // Batterieentladung total [kW]h
-    ['Day_GridBuy_Power Wh',    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   520,     3, 1, 0.01,  0,  true], // Netzbezug heute [kWh]
-    ['Day_GridSell_Power Wh',   VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   521,     3, 1, 0.01,  0,  true], // Einspeisung heute [kWh]
-    ['Total_GridBuy_Power',     VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   522,     3, 1,  0.1,  0,  true], // Gesamter Netzbezug [kWh]
-    ['Total_GridSell_Power',    VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   524,     3, 1,  0.1,  0,  true], // Gesamte Einspeisung [kWh]
-    ['Day_Load_Power ',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   526,     3, 1,  0.1,  0,  true], //  Hausverbrauch  heute [kWh]
-    ['Total_Load_Power',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   527,     3, 2,  0.1,  0,  true], // Gesamtverbrauch [kWh]
-    ['Day_PV_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   529,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute [kWh]
-    ['Day_PV-1_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   530,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 1 [kWh]
-    ['Day_PV-2_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   531,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 2[kWh]
-    ['Day_PV-3_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   532,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 3[kWh]
-    ['Day_PV-4_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Electricity.I', 	   533,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 4[kWh]
-    ['Total	PV_power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    'Electricity.I', 	   534,     3, 1,  0.1,  0,  true], // Solarerzeugung Gesamt [kWh]
+    ['Day_ActGen_Power',      	VARIABLETYPE_FLOAT,     VALTYPE_SWORD,	    '~Electricity', 	   501,     3, 1,  0.1,  0,  true], // Gesamterzeugung heute 
+    ['Day_Bat_Charge',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   514,     3, 1,  0.1,  0,  true], // //Batterieladung heute kWh
+    ['Total_Bat_Charge',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   516,     3, 2,  0.1,  0,  true], // Batterieladung gesamt [kWh]
+    ['Total_Bat_Discharge',     VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   518,     3, 2, 0.01,  0,  true], // Batterieentladung total [kW]h
+    ['Day_GridBuy_Power Wh',    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   520,     3, 1, 0.01,  0,  true], // Netzbezug heute [kWh]
+    ['Day_GridSell_Power Wh',   VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   521,     3, 1, 0.01,  0,  true], // Einspeisung heute [kWh]
+    ['Total_GridBuy_Power',     VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   522,     3, 1,  0.1,  0,  true], // Gesamter Netzbezug [kWh]
+    ['Total_GridSell_Power',    VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   524,     3, 1,  0.1,  0,  true], // Gesamte Einspeisung [kWh]
+    ['Day_Load_Power ',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   526,     3, 1,  0.1,  0,  true], //  Hausverbrauch  heute [kWh]
+    ['Total_Load_Power',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   527,     3, 2,  0.1,  0,  true], // Gesamtverbrauch [kWh]
+    ['Day_PV_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   529,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute [kWh]
+    ['Day_PV-1_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   530,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 1 [kWh]
+    ['Day_PV-2_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   531,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 2[kWh]
+    ['Day_PV-3_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   532,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 3[kWh]
+    ['Day_PV-4_Power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Electricity', 	   533,     3, 1,  0.1,  0,  true], //  Solarerzeugung heute String 4[kWh]
+    ['Total	PV_power Wh',      	VARIABLETYPE_FLOAT,     VALTYPE_DWORD,	    '~Electricity', 	   534,     3, 1,  0.1,  0,  true], // Solarerzeugung Gesamt [kWh]
     ['DC_Trans_Temp', 	        VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Temperature', 	   540,     3, 1, 0.1,   1000,  true], //Transprmatortemperatur [°C]
     ['Heatsink_Temp', 	        VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Temperature', 	   541,     3, 1, 0.1,   1000,  true], //Kühlkörpertemperatur [°C]
     ['Bat_Temp',    	        VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Temperature', 	   586,     3, 1, 0.1,   1000,  true], //Batterietemperatur [°C]
-    ['Bat_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   587,     3, 1, 0.01,  0,  true], //Battereispannung [V]
-    ['Bat_Level',      	        VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Intensity.F', 588,     3, 1,    1,  0,  true], //Batterielevel [%]
+    ['Bat_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   587,     3, 1, 0.01,  0,  true], //Battereispannung [V]
+    ['Bat_Level',      	        VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    '~Battery.100', 588,     3, 1,    1,  0,  true], //Batterielevel [%]
     ['Bat_Out_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   590,     3, 1,    1,  0,  true], //Batterieausgangsleistung [W]
-    ['Bat_Out_Curr',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Ampere.I', 	   591,     3, 1, 0.01,  0,  true], //Batterieausgangsstrom [A] 
+    ['Bat_Out_Curr',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Ampere', 	   591,     3, 1, 0.01,  0,  true], //Batterieausgangsstrom [A] 
     ['Bat_Capacyty',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'AmpHour.I', 	   592,     3, 1,    1,  0,  true], //Batteriekapazität [Ah] 
-    ['Grid_L1_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   598,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L1 [V]
-    ['Grid_L2_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   599,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L2 [V]
-    ['Grid_L3_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   600,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L3 [V]
+    ['Grid_L1_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   598,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L1 [V]
+    ['Grid_L2_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   599,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L2 [V]
+    ['Grid_L3_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   600,     3, 1,  0.1,  0,  true], // Netz Spannung Phase L3 [V]
     ['Out_Grid_Total_Power',    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   619,     3, 1,    1,  0,  true], // Gesamtleistung aus Netz [W]
-    ['Inverter_Freq',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Hertz.I', 	   638,     3, 1, 0.01,  0,  true], // //Inverterfequqnz [Hz]
+    ['Inverter_Freq',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Hertz', 	   638,     3, 1, 0.01,  0,  true], // //Inverterfequqnz [Hz]
 //Load
-    ['Load_L1_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   644,     3, 1,  0.1,  0,  true], // Load Spannung Phase L1 [V]
-    ['Load_L2_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   645,     3, 1,  0.1,  0,  true], // Load Spannung Phase L2 [V]
-    ['Load_L3_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   646,     3, 1,  0.1,  0,  true], // Load Spannung Phase L3 [V]
+    ['Load_L1_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt.I', 	   644,     3, 1,  0.1,  0,  true], // Load Spannung Phase L1 [V]
+    ['Load_L2_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt.I', 	   645,     3, 1,  0.1,  0,  true], // Load Spannung Phase L2 [V]
+    ['Load_L3_Voltage',      	VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt.I', 	   646,     3, 1,  0.1,  0,  true], // Load Spannung Phase L3 [V]
     ['Load_L1_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   650,     3, 1,    1,  0,  true], // Load Leistung Phase L1 [W]
     ['Load_L2_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   651,     3, 1,    1,  0,  true], // Load Leistung Phase L2 [W]
     ['Load_L3_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   652,     3, 1,    1,  0,  true], // Load Leistung Phase L3 [W]
@@ -92,13 +77,13 @@ class SUNxxKSG04LP3 extends Deye
     ['PV2_Inp_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   673,     3, 1,    1,  0,  true], // PV2 Eingangsleistung [W]
     ['PV3_Inp_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   674,     3, 1,    1,  0,  true], // PV3 Eingangsleistung [W]
     ['PV4_Inp_Power',      	    VARIABLETYPE_INTEGER,   VALTYPE_WORD,	    'Watt.I', 	   675,     3, 1,    1,  0,  true], // PV4 Eingangsleistung [W]
-    ['DC1_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   676,     3, 1,  0.1,  0,  true], // DC1 Eingangsspannung [V]
-    ['DC1_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Ampere.I', 	   677,     3, 1,  0.1,  0,  true], // DC1 EingangssStrom [A]
-    ['DC2_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   678,     3, 1,  0.1,  0,  true], // DC2 Eingangsspannung [V]
-    ['DC2_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Ampere.I', 	   679,     3, 1,  0.1,  0,  true], // DC2 EingangssStrom [A]
-    ['DC3_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   680,     3, 1,  0.1,  0,  true], // DC3 Eingangsspannung [V]
-    ['DC3_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Ampere.I', 	   681,     3, 1,  0.1,  0,  true], // DC3 EingangssStrom [A]
-    ['DC4_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Volt.I', 	   682,     3, 1,  0.1,  0,  true], // DC4 Eingangsspannung [V]
-    ['DC4_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    'Ampere.I', 	   683,     3, 1,  0.1,  0,  true] // DC4 EingangssStrom [A]
+    ['DC1_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   676,     3, 1,  0.1,  0,  true], // DC1 Eingangsspannung [V]
+    ['DC1_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Ampere', 	   677,     3, 1,  0.1,  0,  true], // DC1 EingangssStrom [A]
+    ['DC2_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   678,     3, 1,  0.1,  0,  true], // DC2 Eingangsspannung [V]
+    ['DC2_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Ampere', 	   679,     3, 1,  0.1,  0,  true], // DC2 EingangssStrom [A]
+    ['DC3_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   680,     3, 1,  0.1,  0,  true], // DC3 Eingangsspannung [V]
+    ['DC3_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Ampere', 	   681,     3, 1,  0.1,  0,  true], // DC3 EingangssStrom [A]
+    ['DC4_Voltage',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Volt', 	   682,     3, 1,  0.1,  0,  true], // DC4 Eingangsspannung [V]
+    ['DC4_Current',      	    VARIABLETYPE_FLOAT,     VALTYPE_WORD,	    '~Ampere', 	   683,     3, 1,  0.1,  0,  true] // DC4 EingangssStrom [A]
 ];
 }
